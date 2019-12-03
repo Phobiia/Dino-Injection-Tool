d68f370b2ab84d3dcc639f9e01aafe331bd3d8ce
Mitchell Krause <mitchell.krause@foxvalleymetrology.com>
2019-12-03 12:33:53 -0600

    fixed tab order

--- TheIsle-DinoInjectionTool/Form2.Designer.cs
+++ TheIsle-DinoInjectionTool/Form2.Designer.cs
@@ -54,28 +54,28 @@
             this.txtPort.Location = new System.Drawing.Point(101, 117);
             this.txtPort.Name = "txtPort";
             this.txtPort.Size = new System.Drawing.Size(100, 20);
-            this.txtPort.TabIndex = 4;
+            this.txtPort.TabIndex = 7;
             // 
             // txtFTPAddress
             // 
             this.txtFTPAddress.Location = new System.Drawing.Point(101, 91);
             this.txtFTPAddress.Name = "txtFTPAddress";
             this.txtFTPAddress.Size = new System.Drawing.Size(100, 20);
-            this.txtFTPAddress.TabIndex = 2;
+            this.txtFTPAddress.TabIndex = 4;
             // 
             // txtUsername
             // 
             this.txtUsername.Location = new System.Drawing.Point(101, 143);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(100, 20);
-            this.txtUsername.TabIndex = 6;
+            this.txtUsername.TabIndex = 9;
             // 
             // txtPassword
             // 
             this.txtPassword.Location = new System.Drawing.Point(101, 169);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
-            this.txtPassword.TabIndex = 8;
+            this.txtPassword.TabIndex = 11;
             this.txtPassword.UseSystemPasswordChar = true;
             // 
             // lblFTP
@@ -84,7 +84,7 @@
             this.lblFTP.Location = new System.Drawing.Point(52, 94);
             this.lblFTP.Name = "lblFTP";
             this.lblFTP.Size = new System.Drawing.Size(43, 13);
-            this.lblFTP.TabIndex = 1;
+            this.lblFTP.TabIndex = 2;
             this.lblFTP.Text = "FTP IP:";
             // 
             // lblPort
@@ -93,7 +93,7 @@
             this.lblPort.Location = new System.Drawing.Point(43, 120);
             this.lblPort.Name = "lblPort";
             this.lblPort.Size = new System.Drawing.Size(52, 13);
-            this.lblPort.TabIndex = 3;
+            this.lblPort.TabIndex = 5;
             this.lblPort.Text = "FTP Port:";
             // 
             // lblUsername
@@ -102,7 +102,7 @@
             this.lblUsername.Location = new System.Drawing.Point(37, 146);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(58, 13);
-            this.lblUsername.TabIndex = 5;
+            this.lblUsername.TabIndex = 8;
             this.lblUsername.Text = "Username:";
             // 
             // lblPassword
@@ -111,7 +111,7 @@
             this.lblPassword.Location = new System.Drawing.Point(37, 172);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(56, 13);
-            this.lblPassword.TabIndex = 7;
+            this.lblPassword.TabIndex = 10;
             this.lblPassword.Text = "Password:";
             // 
             // chkRememberMe
@@ -120,7 +120,7 @@
             this.chkRememberMe.Location = new System.Drawing.Point(69, 227);
             this.chkRememberMe.Name = "chkRememberMe";
             this.chkRememberMe.Size = new System.Drawing.Size(95, 17);
-            this.chkRememberMe.TabIndex = 11;
+            this.chkRememberMe.TabIndex = 14;
             this.chkRememberMe.Text = "Remember Me";
             this.chkRememberMe.UseVisualStyleBackColor = true;
             this.chkRememberMe.CheckedChanged += new System.EventHandler(this.chkRememberMe_CheckedChanged);
@@ -130,7 +130,7 @@
             this.btnSave.Location = new System.Drawing.Point(44, 278);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 13;
+            this.btnSave.TabIndex = 16;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
@@ -141,7 +141,7 @@
             this.btnClear.Location = new System.Drawing.Point(126, 278);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
-            this.btnClear.TabIndex = 14;
+            this.btnClear.TabIndex = 17;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
@@ -152,7 +152,7 @@
             this.lblRequired.Location = new System.Drawing.Point(74, 251);
             this.lblRequired.Name = "lblRequired";
             this.lblRequired.Size = new System.Drawing.Size(84, 13);
-            this.lblRequired.TabIndex = 12;
+            this.lblRequired.TabIndex = 15;
             this.lblRequired.Text = "*Required Fields";
             this.lblRequired.Visible = false;
             // 
@@ -161,7 +161,7 @@
             this.txtConnectPort.Location = new System.Drawing.Point(101, 195);
             this.txtConnectPort.Name = "txtConnectPort";
             this.txtConnectPort.Size = new System.Drawing.Size(100, 20);
-            this.txtConnectPort.TabIndex = 10;
+            this.txtConnectPort.TabIndex = 13;
             // 
             // lblConnectPort
             // 
@@ -169,7 +169,7 @@
             this.lblConnectPort.Location = new System.Drawing.Point(9, 198);
             this.lblConnectPort.Name = "lblConnectPort";
             this.lblConnectPort.Size = new System.Drawing.Size(86, 13);
-            this.lblConnectPort.TabIndex = 9;
+            this.lblConnectPort.TabIndex = 12;
             this.lblConnectPort.Text = "Connection Port:";
             // 
             // cboProfile
@@ -199,7 +199,7 @@
             this.cboServerType.Location = new System.Drawing.Point(55, 54);
             this.cboServerType.Name = "cboServerType";
             this.cboServerType.Size = new System.Drawing.Size(121, 21);
-            this.cboServerType.TabIndex = 16;
+            this.cboServerType.TabIndex = 1;
             this.cboServerType.SelectedIndexChanged += new System.EventHandler(this.CboServerType_SelectedIndexChanged);
             // 
             // btnSelectFolder
@@ -207,7 +207,7 @@
             this.btnSelectFolder.Location = new System.Drawing.Point(55, 120);
             this.btnSelectFolder.Name = "btnSelectFolder";
             this.btnSelectFolder.Size = new System.Drawing.Size(121, 23);
-            this.btnSelectFolder.TabIndex = 17;
+            this.btnSelectFolder.TabIndex = 6;
             this.btnSelectFolder.Text = "Select Folder";
             this.btnSelectFolder.UseVisualStyleBackColor = true;
             this.btnSelectFolder.Visible = false;
@@ -218,7 +218,7 @@
             this.txtSelectedFolder.Location = new System.Drawing.Point(55, 91);
             this.txtSelectedFolder.Name = "txtSelectedFolder";
             this.txtSelectedFolder.Size = new System.Drawing.Size(121, 20);
-            this.txtSelectedFolder.TabIndex = 18;
+            this.txtSelectedFolder.TabIndex = 3;
             this.txtSelectedFolder.Visible = false;
             // 
             // frmOptions
